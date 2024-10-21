Fixed scaling error

Project-2/NN_shots.py
@@ -10,6 +10,8 @@ df = pd.read_csv('england-premier-league-matches-2018-to-2019-stats.csv')
 
 #print(df.head())
 
+df = df.drop(columns=['date_GMT', 'status', 'referee', 'stadium_name', 'home_team_goal_timings', 'away_team_goal_timings'])
+
 '''Målet är att vi ska skapa en modell där vi endast 
 förutse hur många skott laget kommer att göra baserad 
 på opponents lag och om laget är home/away'''
@@ -21,7 +23,7 @@ df['home_team_avg_shots_allowed'] = df.groupby('away_team_name')['home_team_shot
 df['away_team_avg_shots_allowed'] = df.groupby('home_team_name')['away_team_shots'].transform('mean')
 
 #Skapa target variabel om laget är Home eller Away
-df['Total_Shots'] = df.apply(lambda row: row['home_team_shots'] if row['home_team_name'] else row['away_team_shots'], axis=1)
+df['Total_Shots'] = df.apply(lambda row: row['home_team_shots'] if row['home_team_shots'] is not None else row['away_team_shots'], axis=1)
 
 #One Hot code 
 df_one_hot_code = pd.get_dummies(df, columns=['home_team_name', 'away_team_name'])
@@ -32,20 +34,24 @@ df_one_hot_code = pd.get_dummies(df, columns=['home_team_name', 'away_team_name'
 X = df_one_hot_code.drop(['Total_Shots', 'home_team_shots', 'away_team_shots'], axis=1)
 y = df_one_hot_code['Total_Shots']
 
-
-
 #Split train and test med 80% training och 20% testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#Använda bara nummer kolumn innan scaling (non-numeric data error)
-numeric_col = X_train.select_dtypes(include=['number']).columns
-X_train_num = X_train[numeric_col]
-X_test_num = X_test[numeric_col]
+#Verifera nummer av features
+print(f"Shape of X_train after encoding: {X_train.shape}")
+
+# #Använda bara nummer kolumn innan scaling (non-numeric data error)
+# numeric_col = X_train.select_dtypes(include=['number']).columns
+# X_train_num = X_train[numeric_col]
+# X_test_num = X_test[numeric_col]
 
 #Normalisera data
 scaler = StandardScaler()
-X_train_scale = scaler.fit_transform(X_train_num)
-X_test_scale = scaler.transform(X_test_num)
+X_train_scale = scaler.fit_transform(X_train)
+X_test_scale = scaler.transform(X_test)
+
+#Kolla shape av normaliseringsdata - Output: Shape of X_train_scale: (304, 58)
+print(f"Shape of X_train_scale: {X_train_scale.shape}")
 
 '''Steg för skapa modell:
 
@@ -57,6 +63,7 @@ X_test_scale = scaler.transform(X_test_num)
 
 #Skapa modellen med linear
 model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(X_train_scale.shape[1],)),
     tf.keras.layers.Dense(16, activation='relu'),
     tf.keras.layers.Dense(8, activation='relu'),
     tf.keras.layers.Dense(1, activation='linear')
@@ -78,4 +85,4 @@ print(f'Mean Absolute Error test accuracy: {mae}')
 #förutse
 predictions = model.predict(X_test_scale)
 
-print(predictions)
+# print(predictions)