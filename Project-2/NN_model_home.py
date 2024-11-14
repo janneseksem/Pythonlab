990bda08ab8e5732e2ab48cd0135272602e9c735
JannesEksem <danielbui0412@gmail.com>
2024-11-14 11:58:54 +0100

    Missing feature need to be fixed, 3 different model coded in the model for testing purposes

--- Project-2/NN_model_home.py
+++ Project-2/NN_model_home.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
-
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
 
 
 class DataPreperation:
@@ -29,20 +31,7 @@ class DataPreperation:
                 'opponent_shots': self.away_data['opponent_shots']
             })
             y = self.away_data['opponent_shots']
-        # elif target == 'away':
-        #      X = pd.DataFrame({
-        #           'team_shots': self.away_data['team_shots'],
-        #           'opponent_shots': self.home_data['opponent_shots']
-        #      })
-        #      y = self.away_data['team_shots']
-
-        # elif target == 'opponent_away':
-        #      X = pd.DataFrame({
-        #           'team_shots': self.away_data['team_shots'],
-        #           'opponent_shots': self.home_data['opponent_shots']
-
-        #      })
-        #      y = self.home_data['opponent_shots']
+        
         else:
             raise ValueError("Target måste vara 'home' eller 'opponent")
         
@@ -63,32 +52,101 @@ class NeuralNetwork:
     def __init__(self):
         self.model = tf.keras.Sequential([
             tf.keras.Input(shape=(2,)),
-            tf.keras.layers.Dense(100, activation='relu',), 
-            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(10, activation='relu',), 
+            tf.keras.layers.Dense(10, activation='relu'),
             tf.keras.layers.Dense(1, activation='linear')
         ])
         
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.05),
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
                           loss='mean_squared_error',
                           metrics=['mae'])
         
     def train(self, X_train, y_train):
-            self.model.fit(X_train, y_train, epochs=100, verbose=1)
-
+        self.model.fit(X_train, y_train, epochs=100, verbose=1)
+    
+    def relative_accuracy(self, y_true, y_predict):
+        relative_accuracies = [abs(y_t / y_p) / y_t if y_t !=0 else 0 for y_t, y_p in zip(y_true, y_predict)]
+        avg_relative_accuracy = 1 - (sum(relative_accuracies) / len(relative_accuracies))
+        print(f"Neural Network- Relative Accuracy: {avg_relative_accuracy}")
+        
     def evaluate(self, X_test, y_test):
-            y_predict = self.model.predict(X_test)
-            mae = mean_absolute_error(y_test, y_predict)
-            print(f"Neural Network: Mean absolute error Test set: {mae}")
+        y_predict = self.model(X_test)
+        loss, mae = self.model.evaluate(X_test, y_test, verbose=1)
+        print(f"Neural Network- Loss: {loss}, MAE: {mae}")
+        self.relative_accuracy(y_test, y_predict)
             
         
     def predict(self, data):
             return self.model.predict(data)
-        
+
+class DecisionTree:
+    def __init__(self):
+          self.model = DecisionTreeRegressor(random_state=42)
+    
+    def train(self, X_train, y_train):
+         self.model.fit(X_train, y_train)
+    
+    def relative_accuracy(self, y_true, y_predict):
+        relative_accuracies = [abs(y_t / y_p) / y_t if y_t !=0 else 0 for y_t, y_p in zip(y_true, y_predict)]
+        avg_relative_accuracy = 1 - (sum(relative_accuracies) / len(relative_accuracies))
+        print(f"Decision Tree- Relative Accuracy: {avg_relative_accuracy}")
+    
+    def evaluate(self, X_test, y_test):
+        y_predict = self.model(X_test)
+        mae = mean_absolute_error(y_test, y_predict)
+        print(f"Decision Tree- MAE: {mae}")
+        self.relative_accuracy(y_test, y_predict)
+
+    def predict(self, data):
+         return self.model.predict(data)
+
+class RandomForest:
+    def __init__(self):
+          self.model = RandomForestRegressor(random_state=42)
+
+    def train(self, X_train, y_train):
+         self.model.fit(X_train, y_train)
+
+    def relative_accuracy(self, y_true, y_predict):
+        relative_accuracies = [abs(y_t / y_p) / y_t if y_t !=0 else 0 for y_t, y_p in zip(y_true, y_predict)]
+        avg_relative_accuracy = 1 - (sum(relative_accuracies) / len(relative_accuracies))
+        print(f"Random Forest- Relative Accuracy: {avg_relative_accuracy}")
+
+    def evaluate(self, X_test, y_test):
+        y_predict = self.model(X_test)
+        mae = mean_absolute_error(y_test, y_predict)
+        print(f"Random forest- MAE: {mae}")
+        self.relative_accuracy(y_test, y_predict)
+
+    def predict(self, data):
+         return self.model.predict(data)
+
+class KNearestNeighbors:
+    def __init__(self):
+          self.model = KNeighborsRegressor(n_neighbors=5)
+    
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def relative_accuracy(self, y_true, y_predict):
+        relative_accuracies = [abs(y_t / y_p) / y_t if y_t !=0 else 0 for y_t, y_p in zip(y_true, y_predict)]
+        avg_relative_accuracy = 1 - (sum(relative_accuracies) / len(relative_accuracies))
+        print(f"K Nearest- Relative Accuracy: {avg_relative_accuracy}")
+
+    def evaluate(self, X_test, y_test):
+        y_predict = self.model.predict(X_test)
+        mae = mean_absolute_error(y_test, y_predict)
+        print(f"K Nearest- Loss: MAE: {mae}")
+        self.relative_accuracy(y_test, y_predict)
+
+    def predict(self, data):
+         return self.model.predict(data)
+
 def main():
     data_preperation = DataPreperation('home_shots.csv', 'away_shots.csv')
-######### NEURAL NETWORK ############    
-    #Träna och förutse för Home modellen
     X_train_home, X_test_home, y_train_home, y_test_home = data_preperation.load_df(target='home')
+    ######### NEURAL NETWORK ############    
+    #Träna och förutse för Home modellen    
     nn_model = NeuralNetwork()
     nn_model.train(X_train_home, y_train_home)
     nn_model.evaluate(X_test_home, y_test_home)
@@ -99,6 +157,7 @@ def main():
     opponent_nn_model = NeuralNetwork()
     opponent_nn_model.train(X_train_opponent_home, y_train_opponent_home)
     opponent_nn_model.evaluate(x_test_opponent_home, y_test_opponent_home)
+    
 
     #Förutse dagens match med genomsnitt på 2 modeller
     home_avg_shots = data_preperation.home_data['team_shots'].mean()
@@ -118,125 +177,62 @@ def main():
     predict_shots_home = (predict_home_shots + predict_opponent_away_shots) / 2
     print(f"Predicted shots for Osasuna in today's match against Almeria: {predict_shots_home}")
 
-###############################
-######### DECISION TREE ############
-
+    # ###############################
+    # ######### DECISION TREE ############
+    # dt_model = DecisionTree()
+    # dt_model.train(X_train_home, y_train_home)
+    # dt_model.evaluate(X_test_home, y_test_home)
     
-    # away_avg_shots = data_preperation.away_data['team_shots'].mean()
-    # home_avg_opponent_shots = data_preperation.home_data['opponent_shots'].mean()
-    # today_match_data_scaled = data_preperation.scaled_today_match_data(away_avg_shots, home_avg_opponent_shots)
+    # predict_dt_shots = dt_model.predict(today_match_data_scaled)
+    # print(f"Decision Tree prediction for Osasuna: {predict_dt_shots}")
 
-    # predict_away_shots = away_model.predict(today_match_data_scaled)
-    # predict_opponent_away_shots = opponent_away_model.predict(today_match_data_scaled)
+    # ###############################
+    # ######### RANDOM FOREST ############
+    # rf_model = RandomForest()
+    # rf_model.train(X_train_home, y_train_home)
+    # rf_model.evaluate(X_test_home, y_test_home)
+
+    # predict_rf_shots = rf_model.predict(today_match_data_scaled)
+    # print(f"Random Forest prediction for Osasuna: {predict_rf_shots}")
+
+    ###############################
+    #####K-Nearest Neighbors#######
+    knn_model = KNearestNeighbors()
+    knn_model.train(X_train_home, y_train_home)
+    knn_model.evaluate(X_test_home, y_test_home)
+
+    opponent_knn_model = KNearestNeighbors()
+    opponent_knn_model.train(X_train_opponent_home, y_train_opponent_home)
+    opponent_knn_model.evaluate(x_test_opponent_home, y_test_opponent_home)
+
+    predict_knn_shots = knn_model.predict(today_match_data_scaled)
+    predict_knn_opponent = opponent_knn_model.predict(today_match_data_scaled)
+
+    print(f"K Nearest prediction for Osasuna: {predict_knn_shots}")
+    print(f"K Nearest prediction for opponent: {predict_knn_opponent}")
+
+    predict_knn_total = (predict_knn_shots + predict_knn_opponent) / 2
+    print(f"K Nearest predicted shots for Osasuna in today's match against Almeria: {predict_knn_total}") 
     
-    # print("Home average shots: ", away_avg_shots)
-    # print("Away average allow opponent to shoot: ", home_avg_opponent_shots)
-    # print("-----")
-    # print("Model prediction at home: ", predict_away_shots)
-    # print("Model prediction away allow opponent to shoot: ", predict_opponent_home_shots)
-
-    # #Förutse av genomsnitt på både predict
-    # predict_shots_away = (predict_away_shots + predict_opponent_home_shots) / 2
-    # print(f"Predicted shots for Almeria in today's match against Osasuna: {predict_shots_away}")
-
 if __name__ == "__main__":
     main()
 
 
 
-# #Ladda data för specifikt lag i 'home' och 'away' team från csv
-# home_shots = pd.read_csv('home_shots.csv')
-# away_shots = pd.read_csv('away_shots.csv')
+'''1.4 Tekniker i Machine Learning
 
-# '''Målet är att vi ska skapa en modell där vi endast 
-# förutse hur många skott laget kommer att göra baserad 
-# på opponents lag och om laget är home/away'''
+    Neurala nätverk och djupinlärning (deep learning)
+    Beslutsträd och slumpmässiga skogar (decision trees and random forests)
+    Support Vector Machines (SVM)
+    K-Nearest Neighbors (KNN)
+    Bayesianska metoder
 
-# #Definerar labels och Features för hemmaplan, model 1
-# X_home = pd.DataFrame({
-#     'home_shots': home_shots['team_shots'],
-#     'opponent_shots': away_shots['opponent_shots']
-# })
-# #Osasuna faktiska skott i hemmaplan
-# y_home = home_shots['team_shots']
+1.5 Vanliga algoritmer
 
-# #Definerar labels och features för motståndarens skott, model 2
-# X_opponent = pd.DataFrame({
-#     'home_shots': home_shots['team_shots'],
-#     'opponent_shots': away_shots['opponent_shots']
-# })
-# y_opponent = away_shots['opponent_shots']
-
-# #Split train and test med 80% training och 20% testing
-# X_train_home, X_test_home, y_train_home, y_test_home = train_test_split(X_home, y_home, test_size=0.2, random_state=42)
-
-
-# #Normalisera data för model 1
-# scaler_home = StandardScaler()
-# X_train_scale_home = scaler_home.fit_transform(X_train_home)
-# X_test_scale_home = scaler_home.transform(X_test_home)
-
-# #Normalisera data för model 1
-# scaler_home = StandardScaler()
-# X_train_scale_home = scaler_home.fit_transform(X_train_home)
-# X_test_scale_home = scaler_home.transform(X_test_home)
-
-# '''Steg för skapa modell:
-
-# 1. Skapa modellen
-# 2. Kompilera modellen
-# 3. Träna och utvärdera modellen
-
-# '''
-
-# #Skapa modellen med linear
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Dense(100, input_dim=2, activation='relu', ),
-#     tf.keras.layers.Dense(100, activation='relu', ), 
-#     tf.keras.layers.Dense(1, activation='linear')
-# ])
-
-# # Kompilera modellen
-# model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.05), 
-#               loss='mean_squared_error', 
-#               metrics=['mae'])
-
-# #Träna modellen
-# history = model.fit(X_train_scale, y_train, epochs=150)
-
-# #utvärdera modellen
-# y_predict = model.predict(X_test_scale).flatten()
-# mae = mean_absolute_error(y_test, y_predict)
-# print(f"Mean absolute error Test set: {mae}")
-
-# #Förutse
-# model_predict = model.predict(X_test_scale)
-
-# #print(predictions)
-# print("Evaluation matrics: ", model.evaluate(X_test_scale, y_test))
-# print("Predictions: ", y_predict)
-# print("Actual values: ", y_test.values)
-
-
-# #Få medelvärde data för Osasuna hemmaplan och motståndare mot Almeria i bortaplan
-# home_avg_shots = home_shots['team_shots'].mean()
-# away_avg_opponent_shots = away_shots['opponent_shots'].mean()
-
-# #Inputs förutse Osasuna idag
-# today_match_data_home = pd.DataFrame({
-#     'home_shots': [home_avg_shots],
-#     'opponent_shots': [away_avg_opponent_shots]
-# })
-
-
-# print(today_match_data_home)
-# today_match_data_scaled = scaler.transform(today_match_data_home)
-# print(today_match_data_scaled)
-# #Förutse antak skott för dagens match
-# predicted_shots = model.predict(today_match_data_scaled)
-# print(f"Predicted shots for Osasuna in today's match against Almeria: {predicted_shots[0]}")
-
-# '''
-# Nästa steg: Plotta med matplot och förbättra accuracy på modellen
-# '''
-
+    Linjär regression
+    Logistisk regression
+    K-means klustring
+    Principal Component Analysis (PCA)
+    Convolutional Neural Networks (CNN)
+    Recurrent Neural Networks (RNN)
+'''
