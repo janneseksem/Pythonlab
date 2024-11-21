0ac1e2868f14900c8fb24ea555bf72d8bc1dd815
JannesEksem <danielbui0412@gmail.com>
2024-11-21 20:20:41 +0100

    Transfered Random Forest as class

--- Project-2/NN_model_home.py
+++ Project-2/NN_model_home.py
@@ -2,14 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import classification_report
 
-
-'''
-SMALL DATASET?
-'''
 
 class DataPreperation:
     def __init__(self, merged_csv):
@@ -17,39 +15,44 @@ class DataPreperation:
         self.df = pd.read_csv(merged_csv)
 
     def OneHot(self):
-         #Konvertera date till numeric med UNIX timestamp
-         #annars drop om det ej finns nummer kolumn
-        if 'date' in self.df.columns:
-            self.df['date'] = pd.to_datetime(self.df['date'], errors='coerce')
-            self.df['date_numeric'] = self.df['date'].astype(np.int64) // 10**9
-            self.df.drop(columns=['date'], inplace=True)
+        
+        
+        self.df = pd.get_dummies(self.df)
 
-        #One hot koda dataframe
-        self.df = pd.get_dummies(self.df, drop_first=True)
+        X = self.df.drop('home_total_shots', axis=1)
+        y = self.df['home_total_shots']
 
-        # Drop
-        self.df = self.df.select_dtypes(include=[np.number])
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-        #NaN fylls med 0
-        self.df.fillna(0, inplace=True)
+        missing_cols = X_train.columns[X_train.isnull().any()]
+        X_train = X_train.drop(columns=missing_cols)
+        X_test = X_test.drop(columns=missing_cols)
 
-    def load_df(self):
-            self.OneHot()
+        X_train_scaled = pd.DataFrame(self.scaler.fit_transform(X_train), columns=X_train.columns)
+        X_test_scaled = pd.DataFrame(self.scaler.fit_transform(X_test), columns=X_test.columns)
+
+        X_train_scaled.dropna(axis=1, inplace=True)
+        X_test_scaled.dropna(axis=1, inplace=True)
+        return X_train_scaled, X_test_scaled, y_train, y_test
+
+    # def load_df(self):
+    #         self.OneHot()
             
-            X = self.df.drop('home_total_shots', axis=1)
-            y = self.df['home_total_shots']
+    #         X = self.df.drop('home_total_shots', axis=1)
+    #         y = self.df['home_total_shots']
 
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-            X_train_scaled = self.scaler.fit_transform(X_train)
-            X_test_scaled = self.scaler.transform(X_test)
-            return X_train_scaled, X_test_scaled, y_train, y_test
+    #         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    #         X_train_scaled = self.scaler.fit_transform(X_train)
+    #         X_test_scaled = self.scaler.transform(X_test)
+    #         return X_train_scaled, X_test_scaled, y_train, y_test
 
 class NeuralNetwork:
     def __init__(self):
+        
         self.model = tf.keras.Sequential([
             tf.keras.layers.Dense(100, activation='elu',), 
             tf.keras.layers.Dense(100, activation='elu'),
-            tf.keras.layers.Dense(1, activation='linear')
+            tf.keras.layers.Dense(1)
         ])
         
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.05),
@@ -60,66 +63,136 @@ class NeuralNetwork:
         history = self.model.fit(X_train, y_train, epochs=100, verbose=0)
         return history
 
-        
     def evaluate(self, X_test, y_test):
         y_predict = self.model.predict(X_test)
         loss, mae = self.model.evaluate(X_test, y_test, verbose=1)
         r2 = r2_score(y_test, y_predict)
         print(f"Neural Network- Loss: {loss}, MAE: {mae}, r2: {r2}")
-        
-    def predict(self, data):
-            return self.model.predict(data)
+        return y_predict
+    
+class RandomForest:
+    def __init__(self):
+
+        self.param_grid = {
+            'n_estimators': [100, 200, 300],
+            'max_depth': [None, 10, 20, 30],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+         }
+        self.grid_search = None
+
+    def train(self, X_train, y_train):
+        rf = RandomForestRegressor(random_state=42)
+        self.grid_search = GridSearchCV(estimator=rf, param_grid=self.param_grid, cv=5, n_jobs=-1, verbose=2, scoring='r2')
+        self.grid_search.fit(X_train, y_train)
+        return self.grid_search
+    
+    def evaluate(self, X_test, y_test):
+        if self.grid_search is not None:
+             
+            best_model = self.grid_search.best_estimator_
+            y_predict = best_model.predict(X_test)
+            mse = mean_squared_error(y_test, y_predict)
+            print(f"Random Forest- MSE: {mse}")
+        else:
+             print("WARNING: Train the model first")
+    
+
 
 def main():
 
     data_preperation = DataPreperation('fixed_merged_la_liga_results.csv')
-    X_train_home, X_test_home, y_train_home, y_test_home = data_preperation.load_df()
+    X_train_home, X_test_home, y_train_home, y_test_home = data_preperation.OneHot()
 
     # ######## NEURAL NETWORK ############
-    # Neural Network- Loss: 28.1712589263916, 
-    # MAE: 4.265266418457031, r2: 0.11563318967819214   
+    # #Neural Network- Loss: 21.72272300720215, 
+    # #MAE: 3.5236146450042725, r2: 0.40612727403640747
     # #Träna och förutse för Home modellen
-    #    
-    nn_model = NeuralNetwork()
-    nn_history = nn_model.train(X_train_home, y_train_home)
-    nn_model.evaluate(X_test_home, y_test_home)
-    # home_avg_shots = data_preperation.df['team_shots'].mean()
+    
+    # nn_model = NeuralNetwork()
+    # nn_history = nn_model.train(X_train_home, y_train_home)
+    # predict_test_home_shots = nn_model.evaluate(X_test_home, y_test_home)
+    # # home_avg_shots = data_preperation.df['team_shots'].mean()
 
-    #Förutse hela test settet
-    predict_test_home_shots = nn_model.predict(X_test_home)
-    predict_test_home_shots = np.array(predict_test_home_shots)
-    actual_test_home_shots = np.array(y_test_home)
+    # #Förutse hela test settet
+    # predict_test_home_shots = np.array(predict_test_home_shots)
+    # actual_test_home_shots = np.array(y_test_home)
 
-    print("\nPredicted and Actual values (first 10 ex):")
-    print("Predicted:\n", predict_test_home_shots[:10])
-    print("Actual:\n", actual_test_home_shots[:10])
+    # print("\nPredicted and Actual values (first 10 ex):")
+    # print("Predicted:\n", predict_test_home_shots[:10])
+    # print("Actual:\n", actual_test_home_shots[:10])
 
-    # print("Home average shots: ", home_avg_shots)
-    # print("Away average allow opponent to shoot: ", away_avg_opponent_shots)
-    # print("Model prediction away allow opponent to shoot: ", predict_opponent_away_shots)
+    # # print("Home average shots: ", home_avg_shots)
+    # # print("Away average allow opponent to shoot: ", away_avg_opponent_shots)
+    # # print("Model prediction away allow opponent to shoot: ", predict_opponent_away_shots)
 
-    #Förutse av genomsnitt på både predict
-    # predict_shots_home = (predict_home_shots + predict_opponent_away_shots) / 2
-    # print(f"Predicted shots for Osasuna in today's match against Almeria: {predict_shots_home}")
+    # #Förutse av genomsnitt på både predict
+    # # predict_shots_home = (predict_home_shots + predict_opponent_away_shots) / 2
+    # # print(f"Predicted shots for Osasuna in today's match against Almeria: {predict_shots_home}")
 
-    #Plot history (Loss curve)
+    # #Plot history (Loss curve)
+    # plt.figure(figsize=(10,7))
+    # plt.plot(nn_history.history['loss'], label='Loss training')
+    # plt.ylabel("loss")
+    # plt.xlabel("epochs")
+    # plt.legend()
+    # plt.show()
+
+    # #plot training data
+    # xAxis = tf.range(0, len(X_test_home))
+    # xAxis, y_test_home[0:len(X_test_home)]
+    # xAxis = tf.range(0, len(X_test_home))
+    # yAxis = y_test_home.to_numpy()
+    # yAxis = tf.cast(yAxis, tf.float32)
+    # shot_modelPredictYAxis = tf.cast(predict_test_home_shots, tf.float32)
+
+    # plt.figure(figsize=(10,7))
+    # plt.scatter(xAxis, yAxis, c='g', label="Test data")
+    # #plot TEST data
+    # plt.scatter(xAxis, shot_modelPredictYAxis, c='r', label="predictions")
+    # #plot predictions
+    # plt.legend()
+    # plt.show()
+    
+    #########RANDOM FOREST##########
+    rf_model = RandomForest()
+    grid_search = rf_model.train(X_train_home, y_train_home)
+    rf_model.evaluate(X_test_home, y_test_home)
+
+    #Skrivs ut bästa parametrar och score
+    print("Bästa parametrar:", grid_search.best_params_)
+    print("Bästa cross-validation score:", grid_search.best_score_)
+    print(f"Train Accuracy: {grid_search.score(X_train_home, y_train_home):.3f}")
+    print(f"Test Accuracy (r2): {grid_search.score(X_test_home, y_test_home):.3f}")
+
+    #utför bästa modellenn på test datan (X_test)
+    rf_y_predict = grid_search.best_estimator_.predict(X_test_home)
+
+    #Frutse hela test settet
+    rf_y_predict = np.array(rf_y_predict)
+    rf_actual_test_shots = np.array(y_test_home)
+    print("Predicted and actual values (first 10 example):")
+    print("Predicted: \n", rf_y_predict[:10])
+    print("Actual: \n", rf_actual_test_shots[:10])
+
+    #Plotta GridSearchCV resultat med MTS v Hyperparameterna
+    results = pd.DataFrame(grid_search.cv_results_)
     plt.figure(figsize=(10,7))
-    plt.plot(nn_history.history['loss'], label='Loss training')
-    plt.ylabel("loss")
-    plt.xlabel("epochs")
-    plt.legend()
+    plt.scatter(results['param_n_estimators'], results['mean_test_score'], c=results['param_max_depth'], cmap='viridis')
+    plt.colorbar(label='max_depth')
+    plt.xlabel('n_estimators')
+    plt.ylabel('Mean test score')
+    plt.title('GridSearchCV Results')
     plt.show()
 
     #plot training data
     xAxis = tf.range(0, len(X_test_home))
     xAxis, y_test_home[0:len(X_test_home)]
-
     xAxis = tf.range(0, len(X_test_home))
-
     yAxis = y_test_home.to_numpy()
     yAxis = tf.cast(yAxis, tf.float32)
-    shot_modelPredictYAxis = tf.cast(predict_test_home_shots, tf.float32)
-    
+    shot_modelPredictYAxis = tf.cast(rf_y_predict, tf.float32)
+
     plt.figure(figsize=(10,7))
     plt.scatter(xAxis, yAxis, c='g', label="Test data")
     #plot TEST data
@@ -127,27 +200,7 @@ def main():
     #plot predictions
     plt.legend()
     plt.show()
-    
+
 
 if __name__ == "__main__":
     main()
-
-
-
-'''1.4 Tekniker i Machine Learning
-
-    Neurala nätverk och djupinlärning (deep learning)
-    Beslutsträd och slumpmässiga skogar (decision trees and random forests)
-    Support Vector Machines (SVM)
-    K-Nearest Neighbors (KNN)
-    Bayesianska metoder
-
-1.5 Vanliga algoritmer
-
-    Linjär regression
-    Logistisk regression
-    K-means klustring
-    Principal Component Analysis (PCA)
-    Convolutional Neural Networks (CNN)
-    Recurrent Neural Networks (RNN)
-'''
