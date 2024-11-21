3ed507a8d47039ffd8387e61d5c155827165615b
JannesEksem <danielbui0412@gmail.com>
2024-11-21 16:57:20 +0100

    improved NN accuracy

--- Project-2/NN_model_home.py
+++ Project-2/NN_model_home.py
@@ -4,11 +4,8 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
-import statsmodels.api as sm
+
 
 '''
 SMALL DATASET??
@@ -56,27 +53,30 @@ class DataPreperation:
         self.scaler = StandardScaler()
         self.df = pd.read_csv(merged_csv)
 
-    def preprocess_data(self):
-         # Convert the date column to a numerical feature (e.g., UNIX timestamp or drop it)
+    def OneHot(self):
+         #Konvertera date till numeric med UNIX timestamp
+         #annars drop om det ej finns nummer kolumn
         if 'date' in self.df.columns:
             self.df['date'] = pd.to_datetime(self.df['date'], errors='coerce')
-            self.df['date_numeric'] = self.df['date'].astype(np.int64) // 10**9  # UNIX timestamp
+            self.df['date_numeric'] = self.df['date'].astype(np.int64) // 10**9
             self.df.drop(columns=['date'], inplace=True)
 
-        # Drop any remaining non-numeric columns
+        #One hot koda dataframe
+        self.df = pd.get_dummies(self.df, drop_first=True)
+
+        # Drop
         self.df = self.df.select_dtypes(include=[np.number])
 
-        # Handle missing values by filling with 0
+        #NaN fylls med 0
         self.df.fillna(0, inplace=True)
 
     def load_df(self):
-            self.preprocess_data()
+            self.OneHot()
             
-        
             X = self.df.drop('home_total_shots', axis=1)
             y = self.df['home_total_shots']
 
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             X_train_scaled = self.scaler.fit_transform(X_train)
             X_test_scaled = self.scaler.transform(X_test)
             return X_train_scaled, X_test_scaled, y_train, y_test
@@ -84,38 +84,37 @@ class DataPreperation:
 class NeuralNetwork:
     def __init__(self):
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(100, activation='relu',), 
-            tf.keras.layers.Dense(100, activation='relu'),
-            tf.keras.layers.Dense(1)
+            tf.keras.layers.Dense(100, activation='elu',), 
+            tf.keras.layers.Dense(100, activation='elu'),
+            tf.keras.layers.Dense(1, activation='linear')
         ])
         
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.05),
                           loss='mean_squared_error',
                           metrics=['mae'])
         
     def train(self, X_train, y_train):
-        history = self.model.fit(X_train, y_train, batch_size=32, epochs=100, verbose=1)
+        history = self.model.fit(X_train, y_train, epochs=100, verbose=0)
         return history
 
         
     def evaluate(self, X_test, y_test):
         y_predict = self.model.predict(X_test)
-        y_predict = np.array(y_predict, dtype=np.float32)
         loss, mae = self.model.evaluate(X_test, y_test, verbose=1)
         r2 = r2_score(y_test, y_predict)
         print(f"Neural Network- Loss: {loss}, MAE: {mae}, r2: {r2}")
-            
         
     def predict(self, data):
             return self.model.predict(data)
 
-    
 def main():
 
     data_preperation = DataPreperation('fixed_merged_la_liga_results.csv')
     X_train_home, X_test_home, y_train_home, y_test_home = data_preperation.load_df()
 
-    # ######## NEURAL NETWORK ############    
+    # ######## NEURAL NETWORK ############
+    # Neural Network- Loss: 28.1712589263916, 
+    # MAE: 4.265266418457031, r2: 0.11563318967819214   
     # #Träna och förutse för Home modellen
     #    
     nn_model = NeuralNetwork()
